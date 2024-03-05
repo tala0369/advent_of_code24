c9c2771541c488f434d8136257876cf450971100
talin <talin.larson@gmail.com>
2024-03-05 11:41:39 -0800

    p1 part 2

--- 1/trebuchet.py
+++ 1/trebuchet.py
@@ -1,36 +1,49 @@
-
-# Find calibration value of each line in input
-# Calibration value found by combining first and last digit in order per line
-# Return sum of all calibration values
 from pathlib import Path
 from typing import List
 import re
 
+# Find calibration value of each line in input
+# Calibration value found by combining first and last digit in order per line
+# Return sum of all calibration values
+
 FILENAME = "input.txt"
+DIGIT_LIST = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
 
 # Get file saved in same dir
 def get_lines(filename: str) -> List[str]:
     p = Path(__file__).with_name(filename)
     with p.open("r") as f:
-        lines = f.readlines()
+        lines = f.read()
+        lines = lines.splitlines()
     return lines
 
+
+def digit_string_lookup(string_digit: str) -> str:
+    if string_digit in DIGIT_LIST:
+        val = DIGIT_LIST.index(string_digit) + 1
+        return str(val)
+    else:
+        return string_digit
+
+
 def get_calibration_val(line: str) -> int:
-    digit_pattern = re.compile("\d")
-    first_digit = digit_pattern.search(line).group()
-    last_digit = digit_pattern.search(line[::-1]).group()
+    pattern = r"(?=(\d|" + "|".join(s for s in DIGIT_LIST) + "))"
+    digits = re.findall(pattern, line)
+    first_digit = digit_string_lookup(digits[0])
+    last_digit = digit_string_lookup(digits[-1])
     calibration_val = int(first_digit + last_digit)
     return calibration_val
-    
+
 
 def get_calibration_total():
     lines = get_lines(FILENAME)
-    sum = 0
+    total_sum = 0
     for l in lines:
         this_val = get_calibration_val(l)
-        sum += this_val
-    print(f"Sum of calibration vals: {sum}")
-    return sum
+        total_sum += this_val
+    print(f"Sum of calibration vals: {total_sum}")
+    return total_sum
 
 
 get_calibration_total()
